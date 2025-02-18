bcb28e57b0eed9a74a418e0d27a24d3dce6013e8
Marcus <sabaledo27@hotmail.com>
2025-02-18 13:25:32 -0300

    Add the value atribute to the buttons and the event listeners

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,1 +1,12 @@
 "use strict";
+// DOM STUFF
+const buttons = document.querySelectorAll(".btn");
+const displayText = document.querySelector(".display-text");
+// Variables
+let expression = "";
+
+for (const button of buttons) {
+  button.addEventListener("click", function (e) {
+    console.log(button.value);
+  });
+}
